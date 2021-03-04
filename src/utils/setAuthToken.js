e094e6e78d9aa6f6feb88ef92500f98c0966cf35
Rome Bell <romebell5@gmail.com>
2021-03-04 15:54:19 -0800

    setAuthToken: add logic

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -4,7 +4,14 @@ import axios from 'axios';
 // Any routes that are protected will require the JWT in order to access them.
 
 const setAuthToken = (token) => {
-    // Add code here
+    // Apply the token to request header
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = token;
+        console.log('=====> HEADERS');
+        console.log(axios.defaults.headers.common);
+    } else {
+        delete axios.defaults.headers.common['Authorization']
+    }
 }
 
 export default setAuthToken;
