f6a8f9b8ee7b6bb81f89c667a8a1268dfef469b1
Nathan Holm <nathan@wholm.com>
2021-03-11 00:02:03 -0800

    Transaction Account field changes
    - “Nickname” changed to “Name.” Now a required field to reflect changes in database.

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -5,7 +5,7 @@ const { REACT_APP_SERVER_URL } = process.env;
 
 const CreateAccount = () => {
   const [institutionName, setInstitutionName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [name, setName] = useState("");
   const [accountType, setAccountType] = useState("");
   const [accountNumber, setAccountNumber] = useState("");
   const [currency, setCurrency] = useState("604952e93d4a9573dbf9f408");
@@ -14,8 +14,8 @@ const CreateAccount = () => {
     setInstitutionName(event.target.value);
   }
 
-  const handleNickname = (event) => {
-    setNickname(event.target.value);
+  const handleName = (event) => {
+    setName(event.target.value);
   }
 
   const handleAccountType = (event) => {
@@ -26,35 +26,43 @@ const CreateAccount = () => {
     setAccountNumber(event.target.value);
   }
 
-  const handleCurrency = (test) => {
-    setCurrency(test);
+  // Accepts DOM event from Currency Selector Component as callback.
+  const handleCurrency = (event) => {
+    // HTML Select element:
+    // Assign current user selection to currency variable
+    setCurrency(event.target.value);
   }
   
+  // Handle form submission and creation of Transaction Account
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const transactionValues = { currency, institutionName, nickname, accountType, accountNumber };
+    const transactionValues = { currency, institutionName, name, accountType, accountNumber };
       
     try {
       const newTransactionAccount = await axios.post(`${REACT_APP_SERVER_URL}/transaction-accounts/create`, transactionValues);
       console.log(newTransactionAccount.data);
     } catch (error) {
-      const errorsArray = error.response.data
-      for (const e of errorsArray) {
-        console.error(`${e.name}: ${e.message}`)
+      if (typeof error.response.data !== "string") {
+        const errorsArray = error.response.data
+        for (const e of errorsArray) {
+          console.error(`${e.name}: ${e.message}`)
+        }
+      } else {
+        console.error(`${error.name}: ${error.message}`)
       }
     }
   }
   
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
+    <div className="form-group">
+    <label htmlFor="name">Name</label>
+    <input type="text" name="name" value={name} onChange={handleName} className="form-control"/>
+    </div>
+    <div className="form-group">
       <label htmlFor="institutionName">Institution Name</label>
       <input type="text" name="institutionName" value={institutionName} onChange={handleInstitutionName} className="form-control"/>
     </div>
-    <div className="form-group">
-    <label htmlFor="nickname">Nickname</label>
-    <input type="text" name="nickname" value={nickname} onChange={handleNickname} className="form-control"/>
-  </div>
     <div className="form-group">
       <label htmlFor="accountType">Account Type</label>
       <input type="text" name="accountType" value={accountType} onChange={handleAccountType} className="form-control"/>
--- src/components/CurrencySelector.jsx
+++ src/components/CurrencySelector.jsx
@@ -9,22 +9,28 @@ class CurrencySelector extends Component {
     this.handleCurrency = this.handleCurrency.bind(this);
   }
   handleCurrency (event) {
+    // Update State value from user selection.
     this.setState({value: event.target.value});
-    this.props.action(event.target.value);
+    // Return callback event to parent function
+    this.props.action(event);
   }
   
   async componentDidMount() {
+    // Get list of all currency values from server
     const currencyRequest = await axios.get(`${REACT_APP_SERVER_URL}/currencies`);
     
+    // Map currencies to HTML Options elements
     const currencies = currencyRequest.data.map((currency) => {
       const { code, name, _id } = currency;
       return <option key={_id} value={_id}>{code} | {name}</option>
     });
+    // Set State data to array of HTML Option elements
     this.setState({ data: currencies });
   }
   render() {
     return (
-      <select value={this.state.value} defaultValue="604952e93d4a9573dbf9f408" onChange={this.handleCurrency} name="currency">{this.state.data}</select>
+      // Return an HTML Select element with currency list as Options.
+      <select value={this.state.value} onChange={this.handleCurrency} name="currency">{this.state.data}</select>
     );
   }
 }
