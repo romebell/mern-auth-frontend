d19256b8db5e297d94fb1a38569c26b40887a054
Nathan Holm <nathan@wholm.com>
2021-03-14 16:41:22 +0000

    Update Account Card Balances
    - Update Account Cards to display the current balance  and the currency the account is held in.

--- src/components/transaction-account/AccountList.jsx
+++ src/components/transaction-account/AccountList.jsx
@@ -18,7 +18,7 @@ class AccountList extends Component {
     
     // Map an array of account cards.
     const accountCards = await userAccounts.data.map((account, index) => {
-      return <AccountCard key={index} name={account.name} accountNumber={account.account_number} balance="$768" />
+      return <AccountCard key={index} name={account.name} accountNumber={account.account_number} balance={`${account.currency.symbol}${account.balance}`} />
     });
     
     // Set initial display of account cards after promise resolved
