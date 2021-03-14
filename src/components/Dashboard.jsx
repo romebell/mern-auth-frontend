ef0bcd45d3b90b33d86d4ae8f992ba0f25ae01cc
Nathan Holm <nathan@wholm.com>
2021-03-14 09:33:42 -0700

    Changes to Footer and Add Account
    - Footer no longer anchored to viewport
    - Move Footer element from App.js to Index.js to prevent content being cut off by parent div
    - Organize Components with “globals” and “transacion-accounts” folders
    - Change Account Type from text input to selector in Add Account.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AccountList from "./AccountList";
+import AccountList from "./transaction-account/AccountList";
 
 const Dashboard = (props) => {
    const { handleLogout, user } = props;
